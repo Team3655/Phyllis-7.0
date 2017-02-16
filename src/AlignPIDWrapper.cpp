f1ca6c992cf530ce4266845512bc76e4775ff88a
sagnew <sagnew@PRG23658.iisd.local>
2017-02-16 14:12:08 -0500

    Start of PID Wrapper

---
+++ src/AlignPIDWrapper.cpp
@@ -0,0 +1,25 @@
+/*
+ * AlignPIDWrapper.cpp
+ *
+ *  Created on: Feb 16, 2017
+ *      Author: sagnew
+ */
+
+#include <AlignPIDWrapper.h>
+#include <SmartDashboard.h>
+#include "CommandBase.h"
+
+AlignPIDWrapper::AlignPIDWrapper()
+{
+	// Uses DriveTrain
+}
+
+double AlignPIDWrapper::PIDGet()
+{
+	return frc::SmartDashboard::GetNumber("CenterX", -2);
+}
+
+void AlignPIDWrapper::PIDWrite(double output)
+{
+	//CommandBase::drive.get();
+}
---
+++ src/AlignPIDWrapper.h
@@ -0,0 +1,24 @@
+/*
+ * AlignPIDWrapper.h
+ *
+ *  Created on: Feb 16, 2017
+ *      Author: sagnew
+ */
+
+#ifndef ALIGN_PID_WRAPPER_H
+#define ALIGN_PID_WRAPPER_H
+
+#include <PIDOutput.h>
+#include <PIDSource.h>
+
+class AlignPIDWrapper : public frc::PIDSource, public frc::PIDOutput
+{
+private:
+
+public:
+	AlignPIDWrapper();
+	double PIDGet() override;
+	void PIDWrite(double output) override;
+}
+
+#endif // ALIGN_PID_WRAPPER_H
